62b61c75d34d9e5f789f6a7ac8b66b4843b38d38
kronosmichall <kronosmichall@gmail.com>
2025-06-03 11:18:30 +0200

    local + docker db works

--- src/go.mod
+++ src/go.mod
@@ -10,7 +10,8 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.2.1 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
-	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,15 +1,32 @@
-package crawler 
+package main
 
 import (
+	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"regexp"
+	"strings"
+	"time"
+
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"golang.org/x/net/html"
 )
+
+type Result struct {
+	url   string
+	depth uint
+}
+type Page struct {
+	URL   string `bson:"url"`
+	Title string `bson:"title"`
+	Body  string `bson:"body"`
+}
 
 func getWebsiteBody(path string) ([]byte, error) {
 	resp, err := http.Get(path)
@@ -19,14 +36,55 @@ func getWebsiteBody(path string) ([]byte, error) {
 	}
 	defer resp.Body.Close()
 
-	// limitedReader := io.LimitReader(resp.Body, 2000000)
 	data, err := io.ReadAll(resp.Body)
-
 	if err != nil {
 		fmt.Println("Error", err)
-		return nil, err 
-	}	
-	return (data), nil
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func insertToDb(path string, data []byte, client *mongo.Client) error {
+	title := getTitleOrH1(data)
+	page := Page{path, title, string(data)}
+	collection := client.Database("web").Collection("websites")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := collection.InsertOne(ctx, page)
+	return err
+}
+
+func getTitleOrH1(body []byte) string {
+	doc, err := html.Parse(bytes.NewReader(body))
+	if err != nil {
+		return ""
+	}
+
+	var title string
+	var h1 string
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			if n.Data == "title" && title == "" && n.FirstChild != nil {
+				title = strings.TrimSpace(n.FirstChild.Data)
+			}
+			if n.Data == "h1" && h1 == "" && n.FirstChild != nil {
+				h1 = strings.TrimSpace(n.FirstChild.Data)
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+
+	if title != "" {
+		return title
+	}
+	return h1
 }
 
 func getUrls(body []byte) [][]byte {
@@ -43,25 +101,24 @@ func getBaseUrl(path string) (string, error) {
 	u, err := url.Parse(path)
 	if err != nil {
 		return "", err
-	}	
+	}
 	return u.Host, err
 }
 
-type Result struct {
-	url string
-	depth uint
-}
-
-func extractUrlsAndPublish(path string, ch chan Result, depth uint) {
+func extractUrlsAndPublish(path string, ch chan Result, depth uint, client *mongo.Client) {
 	body, err := getWebsiteBody(path)
 	if err != nil {
 		fmt.Println("Skipping site: ", path)
 		return
 	}
+	err = insertToDb(path, body, client)
+	if err != nil {
+		panic(err)
+	}
 
 	urls := getUrls(body)
-	for _,url := range urls {
-		result := Result{string(url), depth+1}
+	for _, url := range urls {
+		result := Result{string(url), depth + 1}
 		ch <- result
 	}
 }
@@ -70,14 +127,21 @@ func bfs(path string, depth uint, limit uint) {
 	ch := make(chan Result)
 	baseRefs := map[string]uint{}
 	refs := map[string]uint{}
-	
-	client, err := mongo.Connect(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		fmt.Println("Could not find uri in env; fallback to localhost")
+		uri = "mongodb://localhost:27017"
+	} else {
+		fmt.Printf("getting uri from env: %s\n", uri)
+	}
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
 
 	go func() {
-		ch <- Result{path,0}
+		ch <- Result{path, 0}
 	}()
 
 	for {
@@ -90,7 +154,7 @@ func bfs(path string, depth uint, limit uint) {
 			if refs[result.url] == 0 && baseRefs[baseUrl] < limit && result.depth < depth {
 				refs[result.url] += 1
 				fmt.Println("Visiting ", result.url)
-				go extractUrlsAndPublish(result.url, ch, result.depth)
+				go extractUrlsAndPublish(result.url, ch, result.depth, client)
 			} else {
 				refs[result.url] += 1
 			}
@@ -103,7 +167,7 @@ func bfs(path string, depth uint, limit uint) {
 
 func main() {
 	fmt.Println("Hello world")
-	d := flag.Uint("d", 0, "depth of bfs search")
+	d := flag.Uint("d", 1, "depth of bfs search")
 	l := flag.Uint("l", 10, "same base url limit of requests")
 	s := flag.String("s", "", "starting url")
 	flag.Parse()
